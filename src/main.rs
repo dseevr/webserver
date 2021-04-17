3374db88c2d597fee1ca0aa9f455236db378676d
Bill Robinson <dseevr@users.noreply.github.com>
2021-04-16 19:16:27 -0700

    opt out of google floc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate rocket;
 
-use rocket_contrib::serve::StaticFiles;
+use rocket::fairing::AdHoc;
+use rocket_contrib::serve::{Options, StaticFiles};
 
 fn main() -> std::io::Result<()> {
     rocket::ignite()
-        .mount("/", StaticFiles::from(std::env::current_dir()?))
+        .attach(AdHoc::on_response("google sucks", |_, resp| {
+            resp.set_raw_header("Permissions-Policy", "interest-cohort=()");
+        }))
+        .mount(
+            "/",
+            StaticFiles::new(std::env::current_dir()?, Options::Index),
+        )
         .launch();
 
     Ok(())
